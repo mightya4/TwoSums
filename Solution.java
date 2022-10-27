082deddadd3daba06be8438511b7cb8d25992524
Akeem <akeem.mighty@gmail.com>
2022-10-26 21:04:10 -0400

    Created Process To Solve Two Sums Challenge

---
+++ Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        for(int i=0; i<nums.length; i++) {
+            for(int j=0; j<nums.length; j++) {
+                if(nums[j] == nums[i]) {
+                }
+            }
+        }
+        return result;
+    }
+}
