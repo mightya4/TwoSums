247cb9aec7a934d546c2ff65dfcd5dd87bc1019a
Akeem <akeem.mighty@gmail.com>
2022-10-27 19:22:47 -0400

    Completed TwoSums Functionality

--- Solution.java
+++ Solution.java
@@ -2,8 +2,11 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
         int[] result = new int[2];
         for(int i=0; i<nums.length; i++) {
-            for(int j=0; j<nums.length; j++) {
-                if(nums[j] == nums[i]) {
+            int targetVal = target - nums[i];
+            for(int j = 0; j < nums.length; j++) {
+                if(targetVal == nums[j] && i!=j) {
+                    result[1] = j;
+                    result[0] = i;
                 }
             }
         }
